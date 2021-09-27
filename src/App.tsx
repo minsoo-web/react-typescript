53fa117510c671ff932806b75ebc0acaf8c438ea
minsoo kim <zlemzlem5656@naver.com>
2021-09-27 15:41:35 +0900

    [추가] style props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
+import { Container } from "./components/Container";
 
 function App() {
   return (
     <div className="App">
-      <Button handleClick={(event, id) => console.log("object", event, id)} />
-      <Input value="" handleChange={event => console.log(event)} />
+      <Container styles={{ border: "1px solid", padding: "1rem" }} />
     </div>
   );
 }
